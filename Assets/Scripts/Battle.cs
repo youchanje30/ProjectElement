d45ed56f2a59d451d7993c840f0827d6e572c66e
psk <77984774+youchanje30@users.noreply.github.com>
2024-02-01 00:06:44 +0900

    낙하 공격 이미지 추가 및 적용

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -187,7 +187,7 @@ public class Battle : MonoBehaviour
         {
             
             SaveManager.instance.ResetData();
-            // SceneManager.LoadScene("Maintown");
+            SceneManager.LoadScene("Main Scene");
         }    
         // StopCoroutine(ShieldGuard());
     }
